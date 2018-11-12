510c45d90cc21e7dfae56878305cae0cada1e7f2
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2018-11-12 20:10:29 -0300

    Aplicação funcionando com alguns componentes e handleClicks pegando. Faltando alterar o design

--- tindog/src/App.js
+++ tindog/src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import Perfil from "./Perfil";
+import Chat from "./Chat";
+import Dogs from "./data.json";
+import DogGallery from "./components/DogGallery";
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Fragment>
+      <div className="c-muted-2 order-1-s order-2-ns ml-auto ml0-m mv3-s mv0-ns">
+      <div className="mh5 dib pointer">
+        <Perfil />
       </div>
+      <div className="mh5 dib pointer">
+        <Chat />
+      </div>
+    </div>
+    <DogGallery Dogs={Dogs}/>
+    </Fragment>
     );
   }
 }
--- tindog/src/components/DogGallery.js
+++ tindog/src/components/DogGallery.js
@@ -1,14 +1,26 @@
-import React, {component} from "react";
-import dogName from "./DogName";
-import dogAge from "./DogAge";
-import dogRace from "./DogRace";
-import likeButton from "./LikeButton";
+import React, {Component} from "react";
+import DogName from "./DogName";
+import DogAge from "./DogAge";
+import DogRace from "./DogRace";
+import LikeButton from "./LikeButton";
 
 export default class DogCard extends Component{
     render(){
         return(
             <div>
-                
+                {this.props.Dogs.map(dog => {
+                    return(
+                        <article key={dog.id}>
+                         <>
+                            <img src={dog.photo}/>
+                            <DogName name={dog.name} />
+                            <DogAge age={dog.age} />
+                            <DogRace race={dog.race} />
+                            <LikeButton></LikeButton>
+                         </>   
+                        </article>
+                    );
+                })}
             </div>
         );
     }
